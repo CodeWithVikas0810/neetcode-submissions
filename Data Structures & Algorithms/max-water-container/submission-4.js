75889967a008e9d2fdcf609d44b5379927ae4727
CodeWithVikas0810 <139254744+CodeWithVikas0810@users.noreply.github.com>
2026-06-16 10:35:21 +0530

    Add: max-water-container - submission-4

---
+++ Data Structures & Algorithms/max-water-container/submission-4.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let left = 0
+        let right = heights.length-1
+        let res = 0
+        while(left < right){
+
+            let maxVol = (right-left) * Math.min(heights[left],heights[right])
+            res = Math.max(res,maxVol)
+            if(heights[left] < heights[right]){
+                left++
+            }
+            else{
+                right--
+            }
+        }
+        return res
+    }
+}
